56b10bb528ed454a4aa691f4663a0f854e12c278
Praveen Vipparthi <praveen@Praveens-MacBook-Air.local>
2022-10-06 19:36:05 +0530

    added new elements

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
 window.addEventListener('load', ()=>{
-        var l3 = document.getElementById("l3");
-        var c = 0;
-        setInterval(() => {
-            console.log('enter')
-            l3.innerHTML = c.toString();
-            c++;
-            console.log('exit')
-        }, 3000);
-    }
+    char = [["R","S"],["e","w"],["a","i"],["c","f"],["t","t"]];
+    arr = document.getElementsByClassName("letter")
+    arr = [...arr]
+    arr.map((ele)=>{
+        ele.style.animationName = "fade"
+    })
+
+    l5 = arr[4];
+    flag = false;
+    i = 1
+    setInterval(() => {
+        x = getComputedStyle(l5).opacity;
+        if(x == 1)
+            flag = true;
+        if(x == 0 && flag){
+            arr.map((ele,j)=>{
+                ele.innerText = char[j][i];
+            })
+            flag = false;
+            i = (i+1)%2
+        }        
+    }, 400);  
+}
 );
